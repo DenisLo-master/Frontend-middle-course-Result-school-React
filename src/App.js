ebe8bd9deff95f36bdfc9b827fde09f3082a9e2d
DENIS LOGINOV <denis.lkg@gmail.com>
2023-03-16 21:42:51 +0600

    Task2

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import Task1 from './components/Task1.jsx';
+import Task2 from './components/Task2.jsx';
 
 function App() {
 
   return (
-    <div style={{ display: "flex", flexDirection: "row", justifyContent: "space-between" }}>
+    <div style={{ display: "flex", flexDirection: "row", justifyContent: "space-around" }}>
       <div>
         <h2>Задание №1</h2>
         <Task1 />
+      </div>
+      <div>
+        <h2>Задание №2</h2>
+        <Task2 />
       </div>
     </div>
   );
---
+++ src/components/useLocalStorage.js
@@ -0,0 +1,32 @@
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
+
+export function useLocalStorage(curToken = null) {
+    const [token, setToken] = useState()
+
+    function getItem() {
+        const saveToken = localStorage.getItem('token')
+        const curToken = saveToken ? saveToken : null
+        setToken(curToken)
+    }
+
+    function setItem(newToken) {
+        const curToken = localStorage.setItem('token', newToken);
+        setToken(curToken)
+    }
+
+    function removeItem() {
+        localStorage.removeItem('token');
+        setToken(null)
+    }
+
+    useEffect(() => {
+        if (curToken) {
+            setItem(curToken)
+        } else {
+            getItem()
+        }
+    }, [curToken])
+
+    return [token, { setItem, removeItem }]
+}
